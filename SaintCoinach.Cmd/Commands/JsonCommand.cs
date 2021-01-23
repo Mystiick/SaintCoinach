fb1e83fc2ef18e440e48f471135ce882944df0ea
Jake Congdon <Mystiick@outlook.com>
2021-01-23 15:41:42 -0600

    Working prototype for exporting Reipes

--- SaintCoinach.Cmd/Commands/JsonCommand.cs
+++ SaintCoinach.Cmd/Commands/JsonCommand.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 using Newtonsoft.Json.Linq;
 
+using SaintCoinach.Ex.Relational.Definition;
+using SaintCoinach.Xiv;
+
 using Tharga.Toolkit.Console;
 using Tharga.Toolkit.Console.Command;
 using Tharga.Toolkit.Console.Command.Base;
@@ -24,7 +27,7 @@ namespace SaintCoinach.Cmd.Commands {
 
         public override async Task<bool> InvokeAsync(string paramList) {
             const string JsonFileFormat = "json/{0}.json";
-            
+
             IEnumerable<string> filesToExport;
 
             if (string.IsNullOrWhiteSpace(paramList))
@@ -42,27 +45,70 @@ namespace SaintCoinach.Cmd.Commands {
                     if (!target.Directory.Exists)
                         target.Directory.Create();
 
-                    var jo = new List<JObject>();
+                    var output = new List<JObject>();
 
-                    foreach (var sh in sheet) {
-                        var jo2 = new JObject();
-                        
-                        foreach (var col in sheet.Header.Columns) {
-                            //Console.WriteLine($"[{col.Index}] {col}: {sh.GetRaw(col.Index)}");
-                            if (col.Name != null && sh[col.Index] != null) {
-                                jo2.Add(col.Name, sh[col.Index].ToString());
+                    foreach (XivRow row in sheet) {
+                        var record = new JObject();
+
+                        if (row[0].ToString() == "0") {
+                            continue;
+                        }
+
+                        for (int i = 0; i < sheet.Header.Columns.Count(); i++) {
+                            var col = sheet.Header.Columns.ElementAt(i);
+
+                            if (col.Name != null) {
+                                var columnName = CleanColumnName(col.Name);
+
+                                if (col.ValueType == "Item" && sheet.Header.Columns.ElementAt(i + 1).Name.ToLower().StartsWith("amount")) {
+                                    var amountCol = sheet.Header.Columns.ElementAt(++i);
+
+                                    if (row[col.Index] != null) {
+                                        var itemName = row[col.Index].ToString();
+                                        var itemAmount = row[amountCol.Index].ToString();
+
+                                        if (string.IsNullOrWhiteSpace(itemName) && (string.IsNullOrWhiteSpace(itemAmount) || itemAmount == "0")) {
+                                            continue;
+                                        }
+
+                                        var item = new JObject {
+                                                { "item", itemName },
+                                                { "amount",  itemAmount}
+                                            };
+
+                                        if (col.Definition.InnerDefinition.GetType() == typeof(RepeatDataDefinition) && ((RepeatDataDefinition)col.Definition.InnerDefinition).RepeatCount > 1) {
+                                            // First try to get the existing item
+                                            JArray itemArray;
+                                            if (record.ContainsKey(columnName)) {
+                                                itemArray = (JArray)record[columnName];
+                                            }
+                                            else {
+                                                itemArray = new JArray();
+                                                record.Add(columnName, itemArray);
+                                            }
+
+                                            itemArray.Add(item);
+                                        }
+                                        else {
+                                            record.Add(columnName, item);
+                                        }
+                                    }
+                                }
+                                else if(row[col.Index] != null) {
+                                    record.Add(columnName, row[col.Index].ToString());
+                                }
+
                             }
                         }
-                        jo.Add(jo2);
+                        output.Add(record);
                     }
 
-#if DEBUG
-                    var formatting = Newtonsoft.Json.Formatting.Indented;
-#else
                     var formatting = Newtonsoft.Json.Formatting.None;
+#if DEBUG
+                    //formatting = Newtonsoft.Json.Formatting.Indented;
 #endif
 
-                    File.WriteAllText(target.FullName, Newtonsoft.Json.JsonConvert.SerializeObject(jo, formatting));
+                    File.WriteAllText(target.FullName, Newtonsoft.Json.JsonConvert.SerializeObject(output, formatting));
 
 
                     ++successCount;
@@ -76,6 +122,13 @@ namespace SaintCoinach.Cmd.Commands {
             OutputInformation("{0} files exported, {1} failed", successCount, failCount);
 
             return true;
+        }
+
+        private string CleanColumnName(string columnName) {
+            if (columnName.Contains("{")) columnName = columnName.Replace("{", "");
+            if (columnName.Contains("}")) columnName = columnName.Substring(0, columnName.IndexOf("}"));
+
+            return columnName;
         }
     }
 }
